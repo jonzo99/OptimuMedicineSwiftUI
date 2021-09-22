0031825d689b976724ea5433426cce24cca342bd
Jonzo Jimenez <jonzojimenez@gmail.com>
2021-09-22 19:04:16 -0400

    Initial Commit

---
+++ OptimumSwiftUI/OptimumSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  OptimumSwiftUIApp.swift
+//  OptimumSwiftUI
+//
+//  Created by Jonzo Jimenez on 9/22/21.
+//
+
+import SwiftUI
+
+@main
+struct OptimumSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
