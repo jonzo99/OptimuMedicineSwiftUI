bb5118476c38e7a6c427b465ae9fd94972e4131a
Jonzo Jimenez <jonzojimenez@gmail.com>
2022-06-18 23:54:24 -0400

    Refactored the sideMenu Navigation buttons. Removed the use of if else statements to determine what view to navigate to.

--- OptimumSwiftUI/CreateUserViews/MainSideMenu.swift
+++ OptimumSwiftUI/CreateUserViews/MainSideMenu.swift
@@ -26,18 +26,15 @@ struct MainSideMenu: View {
             ScrollView(.vertical, showsIndicators: false) {
                 VStack(alignment: .leading, spacing: 20) {
                     Divider()
-                    TabButton(title: "Profile", image: "person.fill")
+                    sideMenuButtons(title: "Profile", image: "person.fill", content: ProfileView(viewModel: viewModel))
                     if viewModel.currentUser.costCenter == "Admin" {
                         Divider()
-                        TabButton(title: "New Employee", image: "logo.playstation")
+                        sideMenuButtons(title: "New Employee", image: "logo.playstation", content: UserInfoView().onAppear{ showMenu = false})
                         Divider()
-                        TabButton(title: "New Shift", image: "loupe")
+                        sideMenuButtons(title: "New Shift", image: "loupe", content: NewShiftView().onAppear{ showMenu = false})
                         Divider()
-                        TabButton(title: "New Announcement", image: "calendar")
+                        sideMenuButtons(title: "New Announcement", image: "calendar", content: NewShiftView().onAppear{ showMenu = false})
                     }
-                    
-                    // this is how I can show data depending on the user
-                    //if viewModel.currentUser.lastName != "Jimenez" {
                     Divider()
                     TabButton(title: "LogOut", image: "power")
                     
@@ -109,7 +106,31 @@ struct MainSideMenu: View {
         .padding([.horizontal,.leading])
     }
     
-    // we dont want to use if statments
+    
+    struct sideMenuButtons<Content: View>: View {
+        let title: String
+        let image: String
+        let content: Content
+        var body: some View {
+            NavigationLink {
+                self.content
+                    .navigationBarTitle(title)
+                    .navigationBarTitleDisplayMode(.inline)
+            } label: {
+                HStack(spacing: 14) {
+                    Image(systemName: image)
+                        .resizable()
+                        .renderingMode(.template)
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 22, height: 22)
+                    Text(title)
+                }
+                .foregroundColor(.primary)
+                .frame(maxWidth: .infinity, alignment: .leading)
+            }
+        }
+    }
+    
     @ViewBuilder
     func TabButton(title: String, image: String) -> some View {
         if title == "LogOut" {
@@ -132,126 +153,9 @@ struct MainSideMenu: View {
                 .foregroundColor(.primary)
                 .frame(maxWidth: .infinity, alignment: .leading)
             }
-            
-        } else {
-            NavigationLink {
-                // I would just put a whole view in here for that thing
-                // if text == Calendar than I will go to this view
-                
-                if (title == "Calendar") {
-                    CalendarView(shiftViewModel: shiftViewModel, viewModel: viewModel)
-                    //                        .onAppear(){
-                    //                            showMenu = false
-                    //                        }
-                } else if (title == "All Shifts") {
-                    VStack {
-                        Button("fetch all shifts") {
-                            //shiftViewModel.fetchAllShifts()
-                            //print(shiftViewModel.allShifts)
-                            
-                        }
-                        Button("fetch shifts desc") {
-                            
-                            print(shiftViewModel.CalendarShifts)
-                            
-                            shiftViewModel.fetchShiftMetaData()
-                            print(shiftViewModel.CalendarShifts)
-                        }
-                        List(shiftViewModel.CalendarShifts) { shift in
-                            VStack {
-                                
-                                Text(shift.id)
-                                if shift.shift.count > 0 {
-                                    Text(shift.shift[0].shiftName)
-                                }
-                                
-                                
-                                Text("\(shift.shiftDate)")
-                            }
-                        }
-                        List(shiftViewModel.allShifts) { shift in
-                            VStack {
-                                
-                                Text(shift.shiftName)
-                                
-                                ForEach(shift.jobShifts.sorted(by: <), id: \.key) { key, value in
-                                    HStack {
-                                        Text(key)
-                                        Text(value)
-                                    }
-                                }
-                                
-                                
-                                Text("\(shift.startTime)")
-                                Text("\(shift.endTime)")
-                            }
-                        }
-                    }
-                } else if (title == "Profile") {
-                    ProfileView(viewModel: viewModel)
-                        .navigationBarTitle(title)
-                        .navigationBarTitleDisplayMode(.inline)
-                } else if (title == "New Shift") {
-                    NewShiftView()
-                        .navigationTitle(title)
-                        .navigationBarTitleDisplayMode(.inline)
-                        .onAppear(){
-                            showMenu = false
-                        }
-                } else if (title == "Punch") {
-                    
-                    PunchView()
-                        .navigationBarTitle(title)
-                        .navigationBarTitleDisplayMode(.inline)
-                        .onAppear(){
-                            showMenu = false
-                        }
-                }
-                
-                else if (title == "timer") {
-                    // I can make it always true because I created this before as my home view I will need to fix it to remove this
-                    ContentView(isLoggedIn: .constant(true))
-                    // I think this might make my logic a bit easier all I have to do is view will appear and view will disappear and I just have to keep track of that
-                    // I can also make them into seperate views So I dont have to worry about the things now saving or calling stuff.
-                } else if (title == "New Employee") {
-                    UserInfoView()
-                        .navigationTitle("New Employee")
-                        .navigationBarTitleDisplayMode(.inline)
-                        .onAppear(){
-                            showMenu = false
-                        }
-                } else if (title == "New Announcement") {
-                    NewAnnouncementView(viewModel: viewModel)
-                        .navigationTitle("New Announcemnt")
-                        .navigationBarTitleDisplayMode(.inline)
-                        .onAppear(){
-                            showMenu = false
-                        }
-                }
-                else  {
-                    Text(title)
-                        .navigationTitle(title)
-                }
-                
-                
-            } label: {
-                HStack(spacing: 14) {
-                    Image(systemName: image)
-                        .resizable()
-                        .renderingMode(.template)
-                        .aspectRatio(contentMode: .fill)
-                        .frame(width: 22, height: 22)
-                    Text(title)
-                }
-                .foregroundColor(.primary)
-                .frame(maxWidth: .infinity, alignment: .leading)
-            }
         }
-        
     }
-    
 }
-
 struct MainSideMenu_Previews: PreviewProvider {
     static var previews: some View {
         MainSideMenu(showMenu: .constant(true), viewModel: userViewModel(), shiftViewModel: ShiftsViewModel())
